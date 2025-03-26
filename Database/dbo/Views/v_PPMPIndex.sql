f44644bc2090a1b8a1a970304f85b1a5e487aaf0
Jorge Kean <jkn379@gmail.com>
2025-03-26 11:50:08 +0800

    vppmpindex

---
+++ Database/dbo/Views/v_PPMPIndex.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE VIEW dbo.v_PPMPIndex
+AS
+SELECT a.PPMPId,
+       a.BudgetYear,
+       a.Status,
+       a.PPMPNo,
+       d.Name OfficeName,
+       p.FirstName + ', ' + p.LastName PreparedBy,
+       a.Remarks,
+       a.CreatedDate,
+	   a.IsActive
+FROM dbo.PPMPs a
+    JOIN dbo.UM_Department d
+        ON d.DepartmentId = a.RequestingOfficeId
+    JOIN dbo.UM_Person p
+        ON p.PersonId = a.CreatedByUserId;
